c0d90ffcfde26684163b479d0288a49a05bbe5a1
codesche <codesche@gmail.com>
2023-01-06 00:44:39 +0900

    comment: 주석 추가

--- src/main/java/project/board/member/service/MemberService.java
+++ src/main/java/project/board/member/service/MemberService.java
@@ -4,6 +4,11 @@ import project.board.model.MemberInput;
 
 public interface MemberService {
 
+    /**
+     * 회원 가입
+     * @param parameter
+     * @return
+     */
     boolean register(MemberInput parameter);
 
 }
