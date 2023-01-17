1682c402bf06f7840d4344fdc52b6569c8520171
codesche <codesche@gmail.com>
2023-01-17 23:49:22 +0900

    fix: Board 도메인 수정
    postNumber(게시글 번호) 변수형 변경 int -> Long
    
    기존에 추가한 isDeleteYn 컬럼 삭제

--- src/main/java/project/board/noticeboard/entity/Board.java
+++ src/main/java/project/board/noticeboard/entity/Board.java
@@ -1,8 +1,11 @@
-package project.board.entity;
+package project.board.noticeboard.entity;
 
 import java.time.LocalDateTime;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,11 +18,13 @@ import lombok.NoArgsConstructor;
 @Entity
 public class Board {
     @Id
-    private int postNumber;             // 게시글 번호
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long postNumber;             // 게시글 번호  (int형 -> Long으로 변경 2023-01-13)
     private String title;
     private String userId;
     private LocalDateTime createDt;
     private LocalDateTime updateDt;
+
+    @Lob
     private String content;
-    private int isDeleteYn;             // 게시판 글 삭제 여부 확인
 }
