2b391a33ae0fd83ce2907831c83266be59f1a9f7
codesche <codesche@gmail.com>
2023-01-18 22:22:28 +0900

    refactor: 사용안하는 import 제거

--- src/main/java/project/board/noticeboard/repository/BoardRepository.java
+++ src/main/java/project/board/noticeboard/repository/BoardRepository.java
@@ -1,8 +1,6 @@
 package project.board.noticeboard.repository;
 
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import project.board.entity.Member;
 import project.board.noticeboard.entity.Board;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
