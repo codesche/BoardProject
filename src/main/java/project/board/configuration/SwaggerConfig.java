85709ecda5beadc333da90c71dc776c6afb7de13
codesche <codesche@gmail.com>
2023-01-22 17:48:02 +0900

    fix: 어노테이션 추가 - @Bean

--- src/main/java/project/board/configuration/SwaggerConfig.java
+++ src/main/java/project/board/configuration/SwaggerConfig.java
@@ -1,5 +1,6 @@
 package project.board.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -13,6 +14,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 
+    @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
             .select()
